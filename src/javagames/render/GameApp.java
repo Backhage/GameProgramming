9cda5bcc82eda648462f0fd7577d0b66d65d7a0b
Eric Backhage <eric.backhage@gmail.com>
2017-02-09 14:21:39 +0100

    Chapter 1: Full screen active rendering with page flipping

--- src/javagames/render/GameApp.java
+++ src/javagames/render/GameApp.java
@@ -1,46 +1,67 @@
 package javagames.render;
 
-import java.awt.Canvas;
 import java.awt.Color;
+import java.awt.DisplayMode;
 import java.awt.Graphics;
-import java.awt.event.WindowAdapter;
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.awt.image.BufferStrategy;
 
 import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
 
-import com.sun.glass.events.WindowEvent;
-
 import javagames.util.FrameRate;
 
-public class HelloWorldApp extends JFrame implements Runnable {
+public class GameApp extends JFrame implements Runnable {
 
 	private static final long serialVersionUID = -5612855817410359454L;
 	private FrameRate frameRate;
 	private BufferStrategy bufferStrategy;
 	private volatile boolean running;
 	private Thread gameThread;
+	private GraphicsDevice graphicsDevice;
+	private DisplayMode currentDisplayMode;
 	
-	public HelloWorldApp() {
+	public GameApp() {
 		frameRate = new FrameRate();
 	}
 	
 	protected void createAndShowGUI() {
-		Canvas canvas = new Canvas();
-		canvas.setSize(320, 240);
-		canvas.setBackground(Color.BLACK);
-		canvas.setIgnoreRepaint(true);
-		getContentPane().add(canvas);
-		setTitle("Active rendering");
 		setIgnoreRepaint(true);
-		pack();
+		setUndecorated(true);
+		setBackground(Color.BLACK);
 		
-		setVisible(true);
-		canvas.createBufferStrategy(2);
-		bufferStrategy = canvas.getBufferStrategy();
+		GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+		graphicsDevice = ge.getDefaultScreenDevice();
+		currentDisplayMode = graphicsDevice.getDisplayMode();
+		
+		if (!graphicsDevice.isFullScreenSupported()) {
+			System.err.println("ERROR: Fullscreen not supported.");
+			System.exit(ERROR);
+		}
+		
+		graphicsDevice.setFullScreenWindow(this);
+		graphicsDevice.setDisplayMode(getDisplayMode());
+		
+		createBufferStrategy(2);
+		bufferStrategy = getBufferStrategy();
+		
+		addKeyListener(new KeyAdapter() {
+			public void keyPressed(KeyEvent e) {
+				if (e.getKeyCode() == KeyEvent.VK_ESCAPE) {
+					shutDown();
+				}
+			}
+		});
 		
 		gameThread = new Thread(this);
 		gameThread.start();
+	}
+	
+	private DisplayMode getDisplayMode() {
+		return new DisplayMode(1920, 1080, 32, DisplayMode.REFRESH_RATE_UNKNOWN);
 	}
 	
 	@Override
@@ -74,25 +95,25 @@ public class HelloWorldApp extends JFrame implements Runnable {
 		frameRate.calculate();
 		g.setColor(Color.GREEN);
 		g.drawString(frameRate.getFrameRate(), 30, 30);
+		g.drawString("Press ESC to exit...", 30, 60);
 	}
 	
-	protected void onWindowClosing() {
+	protected void shutDown() {
 		try {
 			running = false;
 			gameThread.join();
+			System.out.println("Game loop stopped.");
+			graphicsDevice.setDisplayMode(currentDisplayMode);
+			graphicsDevice.setFullScreenWindow(null);
+			System.out.println("Display restored.");
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
 		System.exit(0);
 	}
-
+	
 	public static void main(String[] args) {
-		final HelloWorldApp app = new HelloWorldApp();
-		app.addWindowListener(new WindowAdapter() {
-			public void windowClosing(WindowEvent e) {
-				app.onWindowClosing();
-			}
-		});
+		final GameApp app = new GameApp();
 		SwingUtilities.invokeLater(new Runnable() {
 			public void run() {
 				app.createAndShowGUI();
