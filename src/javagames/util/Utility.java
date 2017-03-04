f7d31a591cdf3a5ae3ba149f2007c277f629bed9
Eric Bäckhage <eric.backhage@gmail.com>
2017-03-04 20:02:40 +0100

    Chapter 7: Intersection testing
    Intersection testing of points, axis-aligned bounding boxes, and circles.

--- src/javagames/util/Utility.java
+++ src/javagames/util/Utility.java
@@ -13,8 +13,8 @@ public class Utility {
     }
 
     public static Matrix3x3f createReverseViewport(float worldWidth, float worldHeight, float screenWidth, float screenHeight) {
-        float scaleX = (screenWidth - 1) / worldWidth;
-        float scaleY = (screenHeight - 1) / worldHeight;
+        float scaleX = worldWidth /(screenWidth - 1);
+        float scaleY = worldHeight / (screenHeight - 1);
         float translateX = (screenWidth - 1) / 2.0f;
         float translateY = (screenHeight - 1) / 2.0f;
 
