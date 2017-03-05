d6596f32c5e3c255669da13ede5b568ad7fcf3c4
Eric Backhage <eric.backhage@gmail.com>
2017-03-05 15:37:40 +0100

    Chapter 8: Game Prototype
    Adding the Polygon Wrapper class

---
+++ src/javagames/prototype/PolygonWrapper.java
@@ -0,0 +1,10 @@
+package javagames.prototype;
+
+import javagames.util.Vector2f;
+
+public class PolygonWrapper {
+    private float worldWidth;
+    private float worldHeight;
+    private Vector2f worldMin;
+    private Vector2f worldMax;
+}
